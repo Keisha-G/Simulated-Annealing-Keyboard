de70037ecf49363c64089d0234783efaed60d653
Keisha-G <keishagordon929@gmail.com>
2022-01-18 10:22:09 +1100

    commit

--- app.js
+++ app.js
@@ -24,16 +24,17 @@ var App = (function () {
             options.generateNeighbor    = Keyboards.GenerateNeighbor; // change one queen from current and return number of attacks on neighbour - int variable
             options.acceptNeighbor      = Keyboards.AcceptNeighbor; // fucntion to set current to new neighbour
 
-            Draw.Initialize(); // sets DOMQueens in the Draw object as an array of all the sqaures on the chess board - 'td'
+            //Draw.Initialize(Keyboards.GetCurrentPositions()); // sets DOMQueens in the Draw object as an array of all the sqaures on the chess board - 'td'
             Graph.Initialize((35) / options.coolingFactor, Keyboards.QWERTYCost); // placeholder number for init-temp (35)
             Console.Initialize(); // assigns the DOMConsole attribute in the Console object to the 'console' div
-            // passing options to simulated_annealing.js and calculating current 'temperature' (cost not actually temp)
-            // places queens on board acccording to state
-            // prints current cost to console
-            // draws point on graph
+            SimulatedAnnealing.Initialize(options); // passing options to simulated_annealing.js and calculating current 'temperature' (cost not actually temp)
+            Console.Print('System energy: ', SimulatedAnnealing.GetCurrentEnergy()); // prints current cost to console
+            Graph.Point(SimulatedAnnealing.GetCurrentEnergy()); // draws point on graph
 
             // does a simulated annealing algorithm 'step' every 0.05 seconds
                 // places queens on board acccording to state
+
+            alreadyRunning = true;
         }
     };
 })();
