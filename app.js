9de52d6544ba7942d864b75f2a2c4fb0c9acd3cd
Keisha-G <keishagordon929@gmail.com>
2022-01-18 21:55:36 +1100

    commit

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var App = (function () {
             options.generateNeighbor    = Keyboards.GenerateNeighbor(); // change one queen from current and return number of attacks on neighbour - int variable
             options.acceptNeighbor      = Keyboards.AcceptNeighbor(); // fucntion to set current to new neighbour
             options.newSolution         = Keyboards.GetCurrentPositions();
+            console.log(options.newSolution);
 
             Draw.Initialize(Keyboards.GetCurrentPositions()); // sets DOMQueens in the Draw object as an array of all the sqaures on the chess board - 'td'
             Graph.Initialize((35) / options.coolingFactor, Keyboards.QWERTYCost); // placeholder number for init-temp (35)
@@ -31,6 +32,22 @@ var App = (function () {
             SimulatedAnnealing.Initialize(options); // passing options to simulated_annealing.js and calculating current 'temperature' (cost not actually temp)
             Console.Print('System energy: ', SimulatedAnnealing.GetCurrentEnergy()); // prints current cost to console
             Graph.Point(SimulatedAnnealing.GetCurrentEnergy()); // draws point on graph
+            /*
+            intervalId = setInterval(function () { // does a simulated annealing algorithm 'step' every 0.05 seconds
+                var done = SimulatedAnnealing.Step();
+                Draw.DrawBoard(Queens.GetCurrentPositions()); // places queens on board acccording to state
+                Console.Print('System energy: ', SimulatedAnnealing.GetCurrentEnergy(),
+                              '&nbsp;&nbsp;&nbsp;',
+                              'System temperature:', SimulatedAnnealing.GetCurrentTemperature());
+                Graph.Point(SimulatedAnnealing.GetCurrentEnergy());
+
+                if (done === true) {
+                    clearInterval(intervalId);
+                    alreadyRunning = false;
+                }
+
+            }, 50);
+            */
 
             alreadyRunning = true;
         }
